d59399b3028080d6b44bc2a9664fbb4ebb65923e
Chonamalus <chonamalus.bin@protonmail.com>
2023-10-16 14:19:16 +0200

    it works now, but difficult to draw

--- lepython/974BZH.py
+++ lepython/974BZH.py
@@ -8,7 +8,7 @@ class MazeGenerator:
     def __init__(self, width_nbrCells, height_nbrCells):
         self.width_nbrCells = width_nbrCells
         self.height_nbrCells = height_nbrCells
-        self.visited_cells = np.full((height_nbrCells,width_nbrCells), False, dtype=bool)
+        self.visited_cells = np.full((width_nbrCells,height_nbrCells), False, dtype=bool)
         self.path_taken = []
         self.current_cell = (0,0)
         self.previous_cell = None
@@ -20,7 +20,8 @@ class MazeGenerator:
             is_insideWinY = y>=0 and y<self.height_nbrCells
             if is_insideWinX and is_insideWinY:
                 is_cellFree = not self.visited_cells[cell]
-                return is_cellFree
+                is_notPrevious = cell != self.previous_cell
+                return is_cellFree and is_notPrevious
             else:
                 return False
 
@@ -42,21 +43,21 @@ class MazeGenerator:
             # Obtain the new path
             self.previous_cell = self.current_cell
             self.current_cell = theChosenCell
-            print(self.visited_cells)
             print("==> :) CurPrev Cells:", self.current_cell, self.previous_cell)
             print("==> path_taken:", self.path_taken, "\n")
         else:
+            self.visited_cells[self.current_cell] = True
             self.previous_cell = self.current_cell
             self.current_cell = self.path_taken.pop()
             print("==> :( CurPrev Cells:", self.current_cell, self.previous_cell)
             print("==> path_taken:", self.path_taken, "\n")
 
     def is_complete(self):
-        return len(self.path_taken) == self.width_nbrCells*self.height_nbrCells
+        return np.all(self.visited_cells == True)
 
 
 # Draw the graphics of the maze. Outside of the MazeGenerator class
-def draw_maze(current_cell, previous_cell, cell_size, wall_thickness):
+def draw_maze(current_cell, previous_cell, cell_size, wall_thickness):        
 
     (x_current, y_current) = current_cell
     (x_previous, y_previous) = previous_cell if previous_cell!=None else current_cell
@@ -68,12 +69,15 @@ def draw_maze(current_cell, previous_cell, cell_size, wall_thickness):
         cell_size-2*wall_thickness,
         raylib.DARKPURPLE)
     
+    centerX = max(x_current,x_previous)*cell_size
+    centerY = max(y_current,y_previous)*cell_size
+
     raylib.DrawRectangle(   # Draw the junction between both cells
-        (x_previous+x_current)*cell_size//2 +wall_thickness,
-        (y_previous+y_current)*cell_size//2 +wall_thickness,
-        wall_thickness if y_previous==y_current else cell_size-2*wall_thickness,
-        wall_thickness if x_previous==x_current else cell_size-2*wall_thickness,
-        raylib.WHITE)
+        centerX -wall_thickness,
+        centerY -wall_thickness,
+        2*wall_thickness,
+        2*wall_thickness,
+        raylib. WHITE)
     
     raylib.DrawRectangle(   # Draw the current cell
         x_current*cell_size+wall_thickness,
@@ -85,11 +89,11 @@ def draw_maze(current_cell, previous_cell, cell_size, wall_thickness):
 
 # This is the main function
 if __name__ == "__main__":
-    width_nbrCells = 7
-    height_nbrCells = 7
+    width_nbrCells = 10 
+    height_nbrCells = 5
     maze = MazeGenerator(width_nbrCells, height_nbrCells)
 
-    window_title = "Maze Generator"
+    window_title = "Maze Generator" 
     cell_size = 30
     wall_thickness = 2
     raylib.InitWindow(
@@ -106,15 +110,14 @@ if __name__ == "__main__":
         if raylib.IsKeyPressed(raylib.KEY_SPACE):
             if not generation_complete:
                 maze.generate_step()
+                draw_maze(
+                    maze.current_cell, 
+                    maze.previous_cell, 
+                    cell_size, 
+                    wall_thickness)
                 if maze.is_complete():
                     generation_complete = True
                     print("Maze is completed !!!")
-                    print('Visited:', maze.visited_cells)
-        draw_maze(
-            maze.current_cell, 
-            maze.previous_cell, 
-            cell_size, 
-            wall_thickness)
  
         raylib.EndDrawing()
 
@@ -152,4 +155,12 @@ Neighbors: []
 Neighbors: [(4, 1)]
 -> theChosenCell = (4, 1)
 ==> :) CurPrev Cells: (4, 1) (4, 0)
+
+Solution: on backtrack en utilisant la previous cell, lors du test de validité, il faut que les cells qu'on choisit soient différentes de la previous, comme ça on ne retouche pas à celle qu'on a pris juste avant !! Et pas de prise de mémoire trop grande, si le labirhynte est infini
+'''
+
+'''
+Troisième erreur: le dessin de la trace derrière les nouvelles cells
+Le dessin n'était jamais bien placé et j'ai eu beaucoup de mal à le faire.
+Solution: la fonction drawRectangle ne met pas le centre aux deux premières variables, mais le coin haut-gauche
 '''
